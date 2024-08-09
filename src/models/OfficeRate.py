387ba0fe9c7182d91f85f52063f698f96ff4e83c
deymonster <deymonster@yandex.ru>
2024-08-09 14:53:42 +0500

    Rename src folder to wb-franchise-api-client

--- src/models/OfficeRate.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel, Field
-
-
-class OfficeRate(BaseModel):
-    avg_rate: float
-    avg_region_rate: float
-    office_id: int
-
--- src/models/RequestCodeResponse.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class RequestCodeResponse(BaseModel):
-    isPush: bool
-    isSuccess: bool
-    sentToBell:  bool
-    sentToNotifications: bool
-    waitTimeout: int
