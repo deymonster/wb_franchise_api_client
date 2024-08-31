c999f4b45a53bc4b7b1f1c04e464742d3beb234a
deymonster <deymonster@yandex.ru>
2024-08-31 22:39:29 +0500

    Add import TokenResponse

--- wb_franchise_api_client/models/__init__.py
+++ wb_franchise_api_client/models/__init__.py
@@ -1,14 +1,16 @@
-from .TokenResponse import *
-from .RequestCodeResponse import *
 from .AccountData import *
-from .ProceedsResponse import *
-from .RewardResponse import *
-from .ShortageResponse import *
 from .HistoryShortage import *
-from .ShksShortage import *
 from .OfficeRate import *
 from .OfficeSpeed import *
 from .OfficeWorkload import *
 from .OperationsResponse import *
+from .ProceedsResponse import *
+from .RequestCodeResponse import *
+from .RewardResponse import *
+from .ShksShortage import *
+from .ShortageResponse import *
+from .TokenResponse import *
+
+
 
 
